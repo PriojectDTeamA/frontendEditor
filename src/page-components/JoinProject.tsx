3701885736fef259acf1b974f11b620225ee040a
0952491 <0952491@hr.nl>
2022-04-19 13:28:51 +0200

    Formatting aangepast in page components

--- src/page-components/JoinProject.tsx
+++ src/page-components/JoinProject.tsx
@@ -2,36 +2,34 @@ import React from "react";
 
 import "./login.css";
 
-
-
-
-
-
-
 export class JoinProject extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="wrapper fadeInDown">
+          <div id="formContent">
+            <div className="fadeIn first"></div>
 
-    render() {
-        return (
-            <div>
-                <div className="wrapper fadeInDown">
-                    <div id="formContent">
+            <form>
+              <input
+                type="text"
+                id="projname"
+                className="fadeIn second"
+                name="newproj"
+                placeholder="Project Name"
+              ></input>
 
-                        <div className="fadeIn first">
-                        </div>
-
-                        <form>
-                            <input type="text" id="projname" className="fadeIn second" name="newproj" placeholder="Project Name"></input>
-
-                            <input type="submit" className="fadeIn fourth" value="Join Project"></input>
-                        </form>
-                    </div>
-                </div>
-
-            </div>
-
-
-        );
-    }
+              <input
+                type="submit"
+                className="fadeIn fourth"
+                value="Join Project"
+              ></input>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default JoinProject
+export default JoinProject;
--- src/page-components/NewProject.tsx
+++ src/page-components/NewProject.tsx
@@ -2,41 +2,44 @@ import React from "react";
 
 import "./login.css";
 
-
-
-
-
-
-
 export class NewProject extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="wrapper fadeInDown">
+          <div id="formContent">
+            <div className="fadeIn first"></div>
 
-    render() {
-        return (
-            <div>
-                <div className="wrapper fadeInDown">
-                    <div id="formContent">
-
-                        <div className="fadeIn first">
-                        </div>
-
-                        <form>
-                            <input type="text" id="projname" className="fadeIn second" name="newproj" placeholder="Project Name"></input>
-                            <select id="language" name="language" placeholder="Language" className="fadeIn third">
-                                <option value="csharp">C#</option>
-                                <option value="java">Java</option>
-                                <option value="python">Python</option>
-                                <option value="javascript">JavaScript</option>
-                            </select>
-                            <input type="submit" className="fadeIn fourth" value="Create"></input>
-                        </form>
-                    </div>
-                </div>
-
-            </div>
-
-
-        );
-    }
+            <form>
+              <input
+                type="text"
+                id="projname"
+                className="fadeIn second"
+                name="newproj"
+                placeholder="Project Name"
+              ></input>
+              <select
+                id="language"
+                name="language"
+                placeholder="Language"
+                className="fadeIn third"
+              >
+                <option value="csharp">C#</option>
+                <option value="java">Java</option>
+                <option value="python">Python</option>
+                <option value="javascript">JavaScript</option>
+              </select>
+              <input
+                type="submit"
+                className="fadeIn fourth"
+                value="Create"
+              ></input>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default NewProject
+export default NewProject;
