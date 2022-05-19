8539ddb8e1834bd853f4bdf96e97d9ea908975ad
Martijn Straathof <martijnfs@me.com>
2022-05-19 11:35:40 +0200

    rewrote test description

--- src/__tests__/runbutton.test.tsx
+++ src/__tests__/runbutton.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import 'regenerator-runtime/runtime'
 
 describe("<RunButton />", () => {
-    test("should change the redux state of loadingScreenOn to true", async () => {
+    test("should not fail, as nothing is tested", async () => {
       // ???
     });
   });
