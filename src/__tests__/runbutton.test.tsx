651ce645d617550993f8d1c1acfbac2aa787d1ef
Martijn Straathof <martijnfs@me.com>
2022-05-19 11:35:13 +0200

    setup tests

---
+++ src/__tests__/runbutton.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import 'regenerator-runtime/runtime'
+
+describe("<RunButton />", () => {
+    test("should change the redux state of loadingScreenOn to true", async () => {
+      // ???
+    });
+  });
