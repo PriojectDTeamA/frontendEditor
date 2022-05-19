6b3be13e3c5db37fd1e1031d1a9d97982028f88f
Martijn Straathof <martijnfs@me.com>
2022-05-19 11:08:58 +0200

    first try

---
+++ src/__tests__/runCode.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import Run from "../extra-components/Run";
+
+describe("<Run/>", () => {
+    test("When pressed, the loading state should change to true", async () => {
+        expect(true).toBe(true);
+    });
+});
