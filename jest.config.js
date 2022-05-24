acedfd37278a00c2e4d474393aaad1b85f769486
0952491 <0952491@hr.nl>
2022-05-24 15:36:11 +0200

    test dependencies geupdatet

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(css|less|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
+  },
+};
