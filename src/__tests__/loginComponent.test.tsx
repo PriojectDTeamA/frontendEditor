cc85deb8f3fa47e186261e80b03e914c1d993915
0952491 <0952491@hr.nl>
2022-05-25 13:16:25 +0200

    expanded login component test

--- src/__tests__/loginComponent.test.tsx
+++ src/__tests__/loginComponent.test.tsx
@@ -1,21 +1,48 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { render, fireEvent, screen } from "../component-types/test-utils";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
+
 import Login from "../page-components/login";
 
-test("integration tests the app component", async () => {
+// you can run solely this testfile by typing in the console either:
+// npm test -t 'loginComponent'
+// or
+// jest -t 'loginComponent'
+// consequently we can replace 'loginComponent' by any given name from a test (such as 'runbutton' for example)
+
+test("integration tests the login component", async () => {
   render(
     <Router>
       <Login />
     </Router>
   );
 
+  // Arrange
+  const usernameToTest = "test";
+  const passwordToTest = "test";
+  const usernameTextBox = screen.getByPlaceholderText("login");
+  const passwordTextBox = screen.getByPlaceholderText("password");
+  const submitButton = screen.getByDisplayValue("Log In");
+
+  // Act
+
+  // Assert
+
   // how does the component load initially?
-  expect(screen.getByPlaceholderText("login")).toBeInTheDocument();
-  expect(screen.getByPlaceholderText("password")).toBeInTheDocument();
+  expect(usernameTextBox).toBeInTheDocument();
+  expect(passwordTextBox).toBeInTheDocument();
 
   // while a certain action is executed (i.e. typing something), how does the component look now?
+  userEvent.type(usernameTextBox, usernameToTest);
+  expect(usernameTextBox).toHaveDisplayValue(usernameToTest);
 
-  // after a certain action was executed (i.e. typing something), how does the component look now?
+  userEvent.type(passwordTextBox, passwordToTest);
+  expect(passwordTextBox).toHaveDisplayValue(passwordToTest);
+
+  submitButton.click(); // logging in, thus redirecting to the Home page
+
+  // after a certain action was executed (i.e. typing or submitting something), how does the component look now?
+  expect(usernameTextBox).not.toBeInTheDocument();
 });
